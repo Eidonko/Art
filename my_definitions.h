ef8c434c6a0dd11dbaa695387515d3697e196874
Eidon <Eidon@tutanota.com>
2019-10-18 09:19:03 +0200

    Add files via upload
    Added new test, added new #define's in my_definitions.h

--- my_definitions.h
+++ my_definitions.h
@@ -7,7 +7,19 @@
 #define NODE2  2
 #define NODE3  3
 #define NODE4  4
+#define WatchdogTask	5
 #define BACKBONE_TASKID  0
 #define HAS_FAILED 9999
 #define ALL	1
 #define WAKEUP	1
+
+#define ManagerIsAliveTimeout				150000
+#define ThisAgentIsAliveTimeout}			750000
+
+
+#define MyWatchdogTask					5
+#define MyProtectedTask					6
+
+#define MyWatchdogTimeout				1000
+#define TaskToBeNotified				7
+
